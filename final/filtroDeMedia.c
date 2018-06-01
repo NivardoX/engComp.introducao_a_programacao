ea35ba6ce91cd67156a6c44c8874c62e3eab26ab
Nivardo Albuquerque <nivardoti@outlook.com>
2018-06-01 16:27:57 -0300

    Update filtroDeMedia.c

--- final/filtroDeMedia.c
+++ final/filtroDeMedia.c
@@ -2,59 +2,127 @@
 #include <stdlib.h>
 #include <string.h>
 
-void suvizarPixel(unsigned char **M,int x, int y,int m,int n){
+FILE *pgm;
+FILE *pgmSuave;
 
-    unsigned char media;
-
-    media = M[x-1][y-1]/9+M[x-1][y]/9+M[x-1][y+1]/9+M[x][y-1]/9+M[x][y]/9+M[x][y+1]/9+M[x+1][y-1]/9+M[x+1][y]/9+M[x+1][y+1]/9;
-
-    M[x][y] = media;
-}
-
-unsigned char** cria_matriz(int m, int n){
-    int i = 0;
-    unsigned char **matriz_temp;
-    matriz_temp = (unsigned char*)malloc(n * sizeof(unsigned char));
-    while(i != n){
-        matriz_temp[i++] = (unsigned char*)malloc(m*sizeof(unsigned char));
-    }
-    return matriz_temp;
-}
+//protótipos
+//aloca a matriz com a resolucao passada
+unsigned char** criarMatriz(int x,int y);
+//pega os caracteres ASCII da imagem e joga na matriz M
+void lerImagem(	unsigned char M**,int x,int y);
+//faz a suavizacao com janela 3x3, pega a origem de M e poe a saida em M2
+void tratarImagem(unsigned char M2**,unsigned char M**,int x,int y);
+//criar arquivo tradado
+void criarNovoArquivo(unsigned char M2**,char saidaNome*,int x,int y,int lim);
+//bordea com 0
+void bordear(unsigned char M**,int x,int y);
 
 int main(){
-    FILE *pgm;
-    FILE *pgmborda;
+
+	unsigned char M**;
+	
     char pgmNome[30];
-    char pgmbordaNome[30];
+    char pgmSuaveNome[30];
     int m, n, scale, element, limite;
     printf("Digite o nome do arquivo a ser suavizado: ");
     scanf("%s", pgmNome);
     printf("Digite o nome do arquivo a ser criado: ");
-    scanf("%s", pgmbordaNome);
+    scanf("%s", pgmSuaveNome);
 
+	//abre a stream
     pgm = fopen(pgmNome , "r");
     if(pgm == NULL){
-        printf("Não pôde ser aberto");
-        return 0;
-    }
-    fscanf(pgm, "%d %d %d", &m, &n, &scale);
-    unsigned char **M;
-    M = cria_matriz(m+2, n+2);
-    int i, j;
+	   printf("Não pôde ser aberto");
+	   exit(1);
+	}
+	
+	
+	fscanf(pgm,"%s",);
+	
+	//scaneia o tamanho e o lim
+	fscanf(pgm, "%d %d %d", &m, &n, &scale);
+	
+	//cria a matriz e le a imagem.
+	M = criarMatriz(m+2,n+2);
+	bordear(M,m,n);
+	lerImagem(M,m,n);
+	
+	//decleara matriz da imagem de saida
+	unsigned char M2**;
+	M2 = criarMatriz(m,n);
+	
+	//trata a matriz e joga em outra.
+	tratarimagem(M2,M,m,n);		
 
-    for(i = 0; i < n-2; i++){
-        for(j = 0; j < m -2 ; j++){
-            //suvizarPixel(M,i,j,m+2,n+2);
-            printf("%d ",M[i][j]);
-        }
-    }
- 
- 
-    fclose(pgm);
-    fclose(pgmborda);
-    for(i = 0; i < n+2; i++){
-        free(M[i]);
-    }
-    free(M);
-    return 0;
+    pgmSuave = fopen(pgmSuave , "w+");
+    if(pgmSuave == NULL){
+	   printf("Não pôde ser aberto");
+	   exit(1);
+	}
+
+	//cria o arquivo de saida.
+	criarNovoArquivo(M2,pgmSuaveNome,m,n,scale);
+		
+
+	return 0;
+}
+
+
+unsigned char** criarMatriz(int x,int y){
+	unsigned char temp**;
+	
+	temp =(unsigned char **)malloc(x * sizeof(char*));
+	
+	int i;
+	for(i = 0 ; i < x; i++){
+		temp[i] =(unsigned char*) chamalloc(y * sizeof(unsigned char));
+	}
+
+	return temp;
+}
+
+void bordear(unsigned char M**,int x,int y){
+
+	int i;
+	for(i = 0; i < x +2;i++){
+		M[0][i] = '0';
+		M[x+2][0] = '0';
+	}
+
+	for(i = 0; i < y +2;i++){
+		M[i][0] = '0';
+		M[0][y+2] = '0';
+	}
+}
+
+void lerImagem(	unsigned char M**,int x,int y){
+	int i,j;
+	char lixo*;
+	unsigned char temp;
+	for(i = 1 ; i< x;i++){
+		for(j=1 ; j < y;j++){
+		
+			fscanf(pgm,"%c ",temp);
+			if(temp == '#' || temp == '\n'){
+				fgets(lixo,51465654,pgm);
+				j--;
+			}else if(temp == ' '){
+				j--;
+				continue;
+			}else(){
+				M[i][j] = temp;
+			}	
+
+		}
+	}
+}
+void tratarImagem(unsigned char M2**,unsigned char M**,int x,int y){
+	
+	unsigned char media;
+	for(i = 1 ; i< x;i++){
+		for(j=1 ; j < y;j++){
+			media = M[i+1][j+1]/9 +M[i+1][j]/9 +M[i+1][j-1]/9 +M[i][j+1]/9 +M[i][j]/9 +M[i][j-1]/9 +M[i-1][j+1]/9 +M[i-1][j]/9 +M[i-1][j-1]/9; 
+			m[i][j] = media
+		}
+	}
 }
